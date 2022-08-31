baeb6a22a7c24fa6aef464224412061182a8aef3
GEUNA <geuna0204@gmail.com>
2022-09-01 02:07:17 +0900

    [feat] Button, Input element 기본 퍼블리싱
    - 디자인 요소는 시안 나온 뒤에 넣을 예정입니다.

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,7 +1,44 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { a11yHidden } from "styles/mixin";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({
+  children,
+  type,
+  name,
+  onClickHandler,
+  isDisabled,
+  isVisible,
+}) => {
+  return (
+    <StButton
+      type={type}
+      name={name}
+      onClick={onClickHandler}
+      disabled={isDisabled}
+      isVisible={isVisible}
+    >
+      {children}
+    </StButton>
+  );
 };
+
+Button.defaultProps = {
+  children: "BUTTON",
+  name: "",
+  type: "button",
+  size: "",
+  variant: "",
+  onClickHandler: null,
+  isVisible: true,
+  isDisabled: true,
+};
+
+const StButton = styled.button`
+  ${({ isVisible }) =>
+    !isVisible &&
+    css`
+      ${a11yHidden}
+    `};
+`;
 
 export default Button;
--- src/components/elements/Input.jsx
+++ src/components/elements/Input.jsx
@@ -1,7 +1,57 @@
 import styled from "styled-components";
+import { a11yHidden } from "styles/mixin";
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({
+  value,
+  type,
+  id,
+  name,
+  placeholder,
+  width,
+  labelText,
+  isHide,
+  onChangeHandler,
+}) => {
+  return (
+    <StInputContainer>
+      <label htmlFor={id} className={isHide ? "a11y-hidden" : ""}>
+        {labelText}
+      </label>
+      <StInput
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        width={width}
+        onChange={onChangeHandler}
+      />
+    </StInputContainer>
+  );
 };
+
+Input.defaultProps = {
+  id: "",
+  labelText: "",
+  isHide: false,
+  value: "",
+  type: "text",
+  name: "",
+  placeholder: "placeholder",
+  width: "",
+  onChangeHandler: null,
+};
+
+const StInputContainer = styled.div`
+  .a11y-hidden {
+    ${a11yHidden}
+  }
+`;
+
+const StInput = styled.input`
+  ::placeholder {
+    font-size: 16px;
+  }
+`;
 
 export default Input;
