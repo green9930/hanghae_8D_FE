6a8685e10750f64d67c8c47c5a1b7a61575ea800
GEUNA <geuna0204@gmail.com>
2022-09-03 15:32:50 +0900

    [feat] assets 폴더 index.js 파일 추가

---
+++ src/assets/index.js
@@ -0,0 +1,43 @@
+import { ReactComponent as alarm } from "assets/alarm.svg";
+import { ReactComponent as arrow } from "assets/arrow.svg";
+import { ReactComponent as BSalectPurple } from "assets/B_salect_purple.svg";
+import { ReactComponent as BSalectWhite } from "assets/B_salect_white.svg";
+import { ReactComponent as headerLogo } from "assets/headerLogo.svg";
+import { ReactComponent as icon_arrow } from "assets/icon_arrow.svg";
+import { ReactComponent as icon_trash } from "assets/icon_trash.svg";
+import { ReactComponent as icon_x } from "assets/icon_x.svg";
+import { ReactComponent as list } from "assets/list.svg";
+import { ReactComponent as logo_google } from "assets/logo_google.svg";
+import { ReactComponent as logo_kakao } from "assets/logo_kakao.svg";
+import { ReactComponent as logo_naver } from "assets/logo_naver.svg";
+import { ReactComponent as logout } from "assets/logout.svg";
+import { ReactComponent as mainArrow } from "assets/mainArrow.svg";
+import { ReactComponent as myPageLogo } from "assets/myPageLogo.svg";
+import { ReactComponent as ranklist } from "assets/ranklist.svg";
+import { ReactComponent as send_message } from "assets/send_message.svg";
+import { ReactComponent as unregister } from "assets/unregister.svg";
+import { ReactComponent as user } from "assets/user.svg";
+
+const icons = {
+  alarm: alarm,
+  arrow: arrow,
+  BSalectPurple: BSalectPurple,
+  BSalectWhite: BSalectWhite,
+  headerLogo: headerLogo,
+  icon_arrow: icon_arrow,
+  icon_trash: icon_trash,
+  icon_x: icon_x,
+  list: list,
+  logo_google: logo_google,
+  logo_kakao: logo_kakao,
+  logo_naver: logo_naver,
+  logout: logout,
+  mainArrow: mainArrow,
+  myPageLogo: myPageLogo,
+  ranklist: ranklist,
+  send_message: send_message,
+  unregister: unregister,
+  user: user,
+};
+
+export default icons;
