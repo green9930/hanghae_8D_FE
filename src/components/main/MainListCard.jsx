3564fc8f8c7f5de161c0a4892eaa8ca99ed6fd68
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-09-04 23:36:28 +0900

    [design] css 수정

--- src/components/main/MainListCard.jsx
+++ src/components/main/MainListCard.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import handleRankColor from "utils/handleRankColor";
+import { colors } from "styles/theme";
 
 const MainListCard = ({ data }) => {
   const {
@@ -13,22 +15,31 @@ const MainListCard = ({ data }) => {
   } = data;
   return (
     <>
-      <StMainContainer>
+      <StMainContainer selectedPrice={selectedPrice}>
         <StMainListImg src={image} alt="이미지" />
         <StMainDesc>
-          <p>{title.length < 12 ? title : title.slice(0, 12) + "..."}</p>
-          <div>
+          <StTitle selectedPrice={selectedPrice}>
+            {title.length < 12 ? title : title.slice(0, 12) + "..."}
+          </StTitle>
+          <StSeller
+            selectedPrice={selectedPrice}
+            rankColor={handleRankColor(userRank)}
+          >
             <span>{userRank}</span>
             {nickName}
-          </div>
-          {selectedPrice ? (
-            <p>
-              <span>채택가</span>
-              {selectedPrice}원
-            </p>
-          ) : (
-            <p>{price}원</p>
-          )}
+          </StSeller>
+          <StPrice selectedPrice={selectedPrice}>
+            {selectedPrice ? (
+              <div>
+                <StSelectedPrice>채택가</StSelectedPrice>
+                <span>{selectedPrice}</span>원
+              </div>
+            ) : (
+              <div>
+                <span>{price}</span>원
+              </div>
+            )}
+          </StPrice>
         </StMainDesc>
         <StMainProcess process={process}>{process}</StMainProcess>
       </StMainContainer>
@@ -39,6 +50,8 @@ const StMainContainer = styled.div`
   display: flex;
   padding: 15px 35px;
   gap: 10px;
+  color: ${({ selectedPrice }) =>
+    selectedPrice ? `${colors.grey3}` : `${colors.black}`};
 `;
 
 const StMainListImg = styled.img`
@@ -48,10 +61,43 @@ const StMainListImg = styled.img`
 const StMainDesc = styled.div`
   display: flex;
   flex-direction: column;
-  /* width: 163px; */
-  width:80%;
+  width: 80%;
   justify-content: center;
 `;
+const StTitle = styled.p`
+  font-size: 14px;
+  font-weight: 700;
+  letter-spacing: -0.5px;
+`;
+const StSeller = styled.div`
+  font-size: 12px;
+  letter-spacing: -3%;
+  span {
+    color: ${({ selectedPrice, rankColor }) =>
+      selectedPrice ? `${colors.grey3}` : rankColor};
+    padding-right: 4px;
+  }
+`;
+const StPrice = styled.div`
+  div {
+    font-size: 10px;
+    letter-spacing: -0.5px;
+  }
+  span {
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 18px;
+    padding-right: 2px;
+  }
+`;
+
+const StSelectedPrice = styled.p`
+  display: inline-block;
+  color: ${colors.grey3};
+  font-size: 14px;
+  letter-spacing: -5%;
+`;
+
 const StMainProcess = styled.div`
   width: 55px;
   height: 20px;
@@ -59,9 +105,9 @@ const StMainProcess = styled.div`
   text-align: center;
   line-height: 20px;
   border-radius: 30px;
-  background-color: ${(props) =>
-    props.process === "진행중" ? "#FF4040" : "#E3DFFF"};
-  color: ${(props) => (props.process === "진행중" ? "#FFFFFF" : "#9083F7")};
+  background-color: ${({ process }) =>
+    process === "진행중" ? "#FF4040" : "#E3DFFF"};
+  color: ${({ process }) => (process === "진행중" ? "#FFFFFF" : "#9083F7")};
 `;
 
 export default MainListCard;
