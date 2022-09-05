96ae4f349108b6f97c9c2ab212791c16783f98c4
GEUNA <geuna0204@gmail.com>
2022-09-06 03:27:29 +0900

    [feat] cookie 관리 함수 생성

---
+++ src/api/cookies.js
@@ -0,0 +1,14 @@
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+
+export const setCookie = (name, value, expire) => {
+  cookies.set(name, value, {
+    path: "/",
+    // expires: ''
+  });
+};
+
+export const getCookie = (name) => cookies.get(name);
+
+export const removeCookie = (name) => cookies.remove(name, { path: "/" });
