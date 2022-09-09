cc22c98ef686462f5121eb4ebff4c6691c2b88f3
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-09-09 18:32:49 +0900

    [feat] recoil 추가

--- src/api/mainApi.js
+++ src/api/mainApi.js
@@ -1,7 +1,8 @@
 import { instance } from "./axios";
 
-export const getMainCheck = () => {
-  return instance.get("/api/main/list");
+export const getMainCheck = (payload) => {
+  return instance.get(`/api/main/list?category=${payload.category}&process=${payload.process}&pages=${payload.page}
+  &sizes=${payload.size}`);
 };
 export const getRandomCards = () => {
   return instance.get("/api/main/randomcards");
