ffa372ec87fdb57428e40d581944217494c453b1
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-09-09 18:41:39 +0900

    [feat] recoil 추가

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "styles/GlobalStyle";
 import App from "./App";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <GlobalStyle />
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={true} />
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <Suspense fallback={<div>로딩 중~~~~</div>}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <RecoilRoot>
+            <ReactQueryDevtools initialIsOpen={true} />
+            <App />
+          </RecoilRoot>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </Suspense>
   </>
 );
