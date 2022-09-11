6d7f0ce89b2082477eb071dc2cc39870eadc7e6e
GEUNA <geuna0204@gmail.com>
2022-09-11 17:03:50 +0900

    [feat] 라우터 로그인 상태 확인

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import DetailPage from "pages/DetailPage";
 import ErrorPage from "pages/ErrorPage";
 import FormPage from "pages/FormPage";
@@ -9,19 +9,46 @@ import LoginPage from "pages/LoginPage";
 import KakaoLogin from "components/login/KakaoLogin";
 import NaverLogin from "components/login/NaverLogin";
 import GoogleLogin from "components/login/GoogleLogin";
+import { getCookie } from "api/cookies";
+import { useQuery } from "react-query";
 
 const Router = () => {
+  const { isLoading, data: isLogin } = useQuery(
+    "loginState",
+    () => (getCookie("accessToken") ? true : false),
+    {
+      onSuccess: (data) => {
+        // console.log(data);
+      },
+    }
+  );
+
+  if (isLoading) return null;
+  // console.log("ISLOGIN", isLogin);
+
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/login"
+        element={isLogin ? <Navigate to="/" /> : <LoginPage />}
+      />
       <Route path="/user/signin/kakao" element={<KakaoLogin />} />
       <Route path="/user/signin/naver" element={<NaverLogin />} />
       <Route path="/user/signin/google" element={<GoogleLogin />} />
-      <Route path="/form" element={<FormPage />} />
-      <Route path="/detail/:id" element={<DetailPage />} />
+      <Route
+        path="/form"
+        element={isLogin ? <FormPage /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/detail/:id"
+        element={isLogin ? <DetailPage /> : <Navigate to="/login" />}
+      />
       <Route path="/edit/:id" element={<EditPage />} />
-      <Route path="/mypage" element={<MyPage />} />
+      <Route
+        path="/mypage"
+        element={isLogin ? <MyPage /> : <Navigate to="/" />}
+      />
       <Route path="/*" element={<ErrorPage />} />
     </Routes>
   );
