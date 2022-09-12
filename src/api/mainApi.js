6e0450c26774127e6f62555f34752bbf23e2d95e
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-09-12 16:45:32 +0900

    [feat] 무한스크롤 기능 추가

--- src/api/mainApi.js
+++ src/api/mainApi.js
@@ -1,9 +1,18 @@
 import { instance } from "./axios";
 
-export const getMainCheck = (payload) => {
-  return instance.get(`/api/main/list?category=${payload.category}&process=${payload.process}&pages=${payload.page}
-  &sizes=${payload.size}`);
+export const getMainCheck = async ({ category, process, pageParam = 1 }) => {
+  const { data } = await instance.get(
+    `/api/main/list?category=${category}&process=${process}&page=${pageParam}&size=10`
+  );
+  // return data;
+  // let pages;
+  // if (pageParam === 1) {
+  //   pages = [{ content: data }];
+  //   return { pages, nextPage: pageParam + 1, currentPage: pageParam };
+  // }
+  return { data, nextPage: pageParam + 1, currentPage: pageParam };
 };
+
 export const getRandomCards = () => {
   return instance.get("/api/main/randomcards");
 };
