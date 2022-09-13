919eadb44c099503eb9429855cd1bd35f5ca9bff
GEUNA <geuna0204@gmail.com>
2022-09-13 21:14:38 +0900

    [feat] 실시간 알림 기능 구현

--- src/components/login/GoogleLogin.jsx
+++ src/components/login/GoogleLogin.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { setCookie } from "api/cookies";
 import { instance } from "api/axios";
@@ -8,8 +8,13 @@ import { loginState } from "state/atom";
 const GoogleLogin = () => {
   const setIsLogin = useSetRecoilState(loginState);
   const navigate = useNavigate();
+  const [listening, setListening] = useState(false);
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState(null);
+  const [meventSource, msetEventSource] = useState(null);
 
   let code = new URL(window.location.href).searchParams.get("code");
+  let eventSource;
 
   useEffect(() => {
     if (code) {
@@ -20,6 +25,30 @@ const GoogleLogin = () => {
           if (await res.headers.authorization) {
             setCookie("accessToken", res.headers.authorization);
             setCookie("refreshToken", res.headers.refreshtoken);
+            eventSource = new EventSource(
+              `${process.env.REACT_APP_BASE_URL}/api/subscribe`
+            );
+            msetEventSource(eventSource); //구독
+
+            eventSource.onopen = (event) => {
+              console.log("EVENTSOURCE OPENED");
+            };
+            eventSource.onmessage = (event) => {
+              console.log("result", event.data);
+              setData((old) => [...old, event.data]);
+              setValue(event.data);
+            };
+            eventSource.onerror = (event) => {
+              console.log(event.target.readyState);
+              if (event.target.readyState === EventSource.CLOSED) {
+                console.log(
+                  "eventsource closed (" + event.target.readyState + ")"
+                );
+              }
+              eventSource.close();
+            };
+
+            setListening(true);
           }
           window.alert("LOGIN SUCCESS!");
           setIsLogin(true);
@@ -31,6 +60,10 @@ const GoogleLogin = () => {
       };
       google();
     }
+    return () => {
+      eventSource.close();
+      console.log("eventsource closed");
+    };
   }, []);
 
   return null;
