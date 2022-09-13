3f82de0042bcf2298dce06ed6f478c6d7cb4f841
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-09-13 20:06:15 +0900

    [design] 상단 sticky 추가

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,14 +3,31 @@ import React from "react";
 import MainCarousel from "./MainCarousel";
 import MainList from "./MainList";
 import MainSelectBox from "./MainSelectBox";
+import styled from "styled-components";
 
 const Main = () => {
   return (
-    <>
+    <StMainWrap>
       <MainCarousel />
+    <StMainContainer>
       <MainSelectBox />
+    </StMainContainer>
       <MainList />
-    </>
+    </StMainWrap>
   );
 };
+
+const StMainWrap=styled.div`
+position:relative;
+top:64px;
+left:0;
+width:100%;
+`
+const StMainContainer=styled.div`
+position:sticky;
+top:64px;
+left:0;
+
+`
+
 export default Main;
