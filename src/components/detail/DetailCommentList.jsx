834993c70e9f8cdbb6386fdcc26d024522ceacf1
GEUNA <geuna0204@gmail.com>
2022-09-15 03:01:40 +0900

    [fix] DetailPage 수정 중

--- src/components/detail/DetailCommentList.jsx
+++ src/components/detail/DetailCommentList.jsx
@@ -3,8 +3,11 @@ import DetailComment from "components/detail/DetailComment";
 import { useQuery } from "react-query";
 import { getComments } from "api/detailApi";
 import { createRef, useRef } from "react";
+import { useRecoilState } from "recoil";
+import { commentScrollState } from "state/atom";
 
 const DetailCommentList = ({ process, articlesId }) => {
+  const [commentScroll, setCommentScroll] = useRecoilState(commentScrollState);
   const commentRef = useRef(null);
   const commentRefs = [];
 
@@ -17,13 +20,21 @@ const DetailCommentList = ({ process, articlesId }) => {
       onSuccess: (data) => {
         console.log("GET COMMENTS", data.data);
         // console.log("GET ISMYARTICLES ", data.data.data.isMyArticles);
-        // console.log(commentRef.current);
+        console.log(commentRef.current);
         // srcollToBottom();
+        // commentRef.current?.focus();
         // commentRef.current?.scrollIntoView();
         // commentRefs[comments.data.data.comments];
         // commentRef.current?.scrollIntoView({
         //   behavior: "smooth",
         // });
+        // commentRef.current.scrollIntoView({
+        //   behavior: "smooth",
+        //   block: "end",
+        //   inline: "nearest",
+        // });
+        // commentScroll && setCommentScroll(false);
+        // commentScroll && srcollToBottom();
       },
     }
   );
@@ -40,32 +51,31 @@ const DetailCommentList = ({ process, articlesId }) => {
   // });
 
   return (
-    <div>
-      <StCommentList process={process}>
-        {comments?.data.comments.map((comment, index, arr) => {
-          // console.log(index === arr.length - 1);
-          return (
-            <li
-              key={comment.commentsId}
-              // ref={index === arr.length - 1 ? commentRef : null}
-            >
-              <DetailComment
-                commentVal={comment}
-                isMyArticles={comments.data.isMyArticles}
-                articlesId={articlesId}
-              />
-              {/* {index === arr.length - 1 ? (
+    <StCommentList process={process}>
+      {/* <StCommentList process={process} ref={commentScroll ? commentRef : null}> */}
+      {comments?.data.comments.map((comment, index, arr) => {
+        // console.log(index === arr.length - 1);
+        return (
+          <li
+            key={comment.commentsId}
+            // ref={index === arr.length - 1 ? commentRef : null}
+          >
+            <DetailComment
+              commentVal={comment}
+              isMyArticles={comments.data.isMyArticles}
+              articlesId={articlesId}
+            />
+            {/* {index === arr.length - 1 ? (
                 <>
                   <StDiv></StDiv>
                   <StDiv ref={commentRef}></StDiv>
                 </>
               ) : null} */}
-            </li>
-          );
-        })}
-        {/* <StDiv ref={commentRef}></StDiv> */}
-      </StCommentList>
-    </div>
+          </li>
+        );
+      })}
+      {/* <StDiv ref={commentRef}></StDiv> */}
+    </StCommentList>
   );
 };
 
@@ -73,12 +83,16 @@ const StCommentList = styled.ul`
   /* height: 100%; */
   /* min-height: 20vh; */
   /* margin-bottom: 100px; */
-  padding: ${({ process }) =>
-    process === "진행중" ? `0 35px 110px 35px` : `0 35px 26px 35px`};
+  /* padding: ${({ process }) =>
+    process === "진행중" ? `0 35px 110px 35px` : `0 35px 26px 35px`}; */
+  padding: 0 35px;
+  /* margin-bottom: 110px; */
+  display: flex;
+  flex-direction: column;
 `;
 
 const StDiv = styled.div`
-  height: 50px;
+  /* height: 50px; */
 `;
 
 export default DetailCommentList;
