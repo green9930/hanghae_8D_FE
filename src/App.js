d7decb39056eaeef9a7018995447996d6081a0bc
GEUNA <geuna0204@gmail.com>
2022-10-07 20:07:32 +0900

    [fix] App.js react-device-detect 제거

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { BrowserView, MobileView } from "react-device-detect";
 import Router from "shared/Router";
 import styled from "styled-components";
-import { colors } from "styles/theme";
 
 const App = () => {
   return (
-    <>
-      <BrowserView>
-        <ResponsiveContainer>
-          <Router />
-        </ResponsiveContainer>
-      </BrowserView>
-      <MobileView>
-        <Router />
-      </MobileView>
-    </>
+    <ResponsiveContainer>
+      <Router />
+    </ResponsiveContainer>
   );
 };
 
@@ -23,12 +14,6 @@ const ResponsiveContainer = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 100vh;
-  background: linear-gradient(
-    333.21deg,
-    ${colors.mainP} 18.97%,
-    ${colors.subP} 148.99%
-  );
 `;
 
 export default App;
