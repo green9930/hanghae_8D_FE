743fb290cb0649a7cdd507b1f8cfa9eefe67522d
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-10-18 00:22:10 +0900

    [feat] 검색 get 요청 추가

--- src/components/search/SearchList.jsx
+++ src/components/search/SearchList.jsx
@@ -1,82 +1,106 @@
 import Button from "components/elements/Button";
-import Input from "components/elements/Input";
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { colors } from "styles/theme";
 import icons from "assets";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import test01 from "assets/geuna.png";
-import test02 from "assets/memoji.png";
 import MainListCard from "components/main/MainListCard";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+import { searchItems } from "api/searchApi";
+import LoadingMessage from "components/etc/LoadingMessage";
 
 const SearchList = () => {
+  const MIN_LENGTH = 0;
+  const MAX_LENGTH = 20;
   const { PrevLogo } = icons;
+  const inputRef = useRef();
   const navigate = useNavigate();
   const [item, setItem] = useState("");
-  const onSubmitHandler = () => {};
+
   const changeHandler = (e) => {
+    if (e.target.value.length > MAX_LENGTH) return;
     setItem(e.target.value);
   };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (item.trim().length !== MIN_LENGTH) refetch();
+    inputRef.current.focus();
+  };
 
-  const datas = [
+  useEffect(() => {
+    inputRef.current.focus();
+    return () => remove();
+  }, []);
+  /* -------------------------------- 데이터 Read -------------------------------- */
+  const { data, isLoading, refetch, remove } = useQuery(
+    "foundLists",
+    () => searchItems(item),
     {
-      articlesId: 1,
-      nickName: "홍창형",
-      process: "진행중",
-      title: "에어팟 중고",
-      price: "79,000",
-      image: test01,
-      userRank: "B",
-      commentCount: 5,
-    },
-    {
-      articlesId: 2,
-      nickName: "김단비",
-      process: "진행 완료",
-      selectedPrice: "120,000",
-      title: "맥북",
-      price: "100,000",
-      image: test02,
-      userRank: "D",
-      commentCount: 5,
-    },
-  ];
+      refetchOnWindowFocus: false,
+      enabled: false,
+    }
+  );
+
+  if (isLoading) return <LoadingMessage />;
 
   return (
     <StSearchList>
-      <StSearchHeader>
-        <Button
-          variant="image"
-          name="prevLogo"
-          onClickHandler={() => navigate(-1)}
-        >
-          <PrevLogo />
-        </Button>
-        <StSearchForm onSubmit={onSubmitHandler}>
-          <Input
-            onChangeHandler={changeHandler}
-            theme="search"
-            value={item}
-            placeholder="무엇을 찾고 계신가요?"
-          />
-          <Button size="small_box" variant="text" children="검색" />
-        </StSearchForm>
-      </StSearchHeader>
-
-      {datas?.map((data) => (
-        <MainListCard key={data.articlesId} data={data} />
-      ))}
+      <StFirstContainer>
+        <StSearchHeader>
+          <Button
+            variant="image"
+            name="prevLogo"
+            onClickHandler={() => navigate(-1)}
+          >
+            <PrevLogo />
+          </Button>
+          <StSearchForm onSubmit={onSubmitHandler}>
+            <StInput
+              onChange={changeHandler}
+              theme="search"
+              value={item}
+              name="item"
+              placeholder="무엇을 찾고 계신가요?"
+              ref={inputRef}
+            />
+            <Button
+              size="small_box"
+              variant="text"
+              children="검색"
+              type="submit"
+            />
+          </StSearchForm>
+        </StSearchHeader>
+      </StFirstContainer>
+      <StSecondContainer>
+        {data?.data?.map((data) => (
+          <MainListCard key={data.articlesId} data={data} />
+        ))}
+      </StSecondContainer>
     </StSearchList>
   );
 };
 
 const StSearchList = styled.div`
   width: 100%;
-  min-height: 100vh;
   background-color: ${colors.white};
   @media screen and (min-width: 950px) {
     width: 500px;
+  }
+`;
+const StFirstContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 100;
+  background-color: ${colors.white};
+  @media screen and (min-width: 950px) {
+    width: 500px;
+    left: 50%;
+    transform: translate(-50%, 0);
   }
 `;
 const StSearchHeader = styled.div`
@@ -90,10 +114,24 @@ const StSearchHeader = styled.div`
 const StSearchForm = styled.form`
   display: flex;
   align-items: center;
-  flex-grow: 1;
   gap: 10px;
+  flex-grow: 1;
   div {
     flex-grow: 1;
   }
+`;
+const StInput = styled.input`
+  height: 40px;
+  border: none;
+  border-radius: 5px;
+  padding: 0 14px;
+  background: ${colors.grey5};
+  width: 100%;
+`;
+const StSecondContainer = styled.div`
+  position: relative;
+  top: 64px;
+  min-height: 100vh;
+  background: ${colors.white};
 `;
 export default SearchList;
